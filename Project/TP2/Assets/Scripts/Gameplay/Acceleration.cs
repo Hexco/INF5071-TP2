0012c3b3624bce50eab1056c45a468cc2bd721ed
Nick P <nicolas.papanicolaou20@gmail.com>
2016-11-09 18:27:34 -0500

    Adder des shits

--- Project/TP2/Assets/Scripts/Gameplay/Acceleration.cs
+++ Project/TP2/Assets/Scripts/Gameplay/Acceleration.cs
@@ -2,8 +2,24 @@
 using System.Collections;
 
 public class Acceleration : MonoBehaviour {
+
+	void Update(){
+		RotatePowerUp ();
+	}
+
     void OnTriggerEnter(Collider other)
     {
+		PickUpSound ();
         Movement.speed += 1;
+		Destroy (this.gameObject);
     }
+		
+	void RotatePowerUp(){
+		this.transform.Rotate (new Vector3 (0,1.5f,0));
+	}
+
+	void PickUpSound(){
+		AudioSource sound = GameObject.Find ("SpeedPickUP").GetComponent<AudioSource> ();
+		sound.Play ();
+	}
 }
