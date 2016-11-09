a1c1d0107b3a14e46293303acc3f13e030d96af4
hexco <hexco.kp@gmail.com>
2016-11-08 19:28:22 -0500

    First
    # Scene de menu fonctionnel
    # Control fonctionnel
    # Scene de base started
    
    Rien de final, mais c'est deja sa!

---
+++ Project/TP2/Assets/Scripts/Menu/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	public void ChangeToScene (int sceneID) {
+        SceneManager.LoadScene(sceneID);
+    }
+}
---
+++ Project/TP2/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float speed = 1;
+    public Rigidbody player;
+    public Vector3 player_movement;
+
+    // Use this for initialization
+    void Start () {
+        player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        var player_rotation = transform.rotation;
+
+        //Movement
+        if (SystemInfo.supportsGyroscope)
+        {
+            print("WHAT");
+        } else
+        {
+            if(Input.GetKey("w"))
+            {
+                player_movement.Set(10 * speed, -5*speed, player_movement.z);
+                transform.rotation = player_rotation * Quaternion.Euler(0,0,1);
+            }
+            else if (Input.GetKey("s"))
+            {
+                player_movement.Set(10 * speed, 5*speed, player_movement.z);
+                transform.rotation = player_rotation * Quaternion.Euler(0, 0, -1);
+            } else
+            {
+                player_movement.Set(10 * speed, 0, player_movement.z);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                player_movement.Set(10 * speed, player_movement.y, 5 * speed);
+                transform.rotation = player_rotation * Quaternion.Euler(1, 0, 0);
+            }
+            else if (Input.GetKey("d"))
+            {
+                player_movement.Set(10 * speed, player_movement.y, -5 * speed);
+                transform.rotation = player_rotation * Quaternion.Euler(-1, 0, 0);
+            }
+            else
+            {
+                player_movement.Set(10 * speed, player_movement.y, 0);
+            }
+            player.velocity = player_movement;
+        }
+    }
+}
