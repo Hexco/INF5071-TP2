77837ee68eaee081b169fd12cfc24abd8ec47d8f
hexco <hexco.kp@gmail.com>
2016-11-08 23:32:45 -0500

    controle/scene

--- Project/TP2/Assets/Scripts/Player/Movement.cs
+++ Project/TP2/Assets/Scripts/Player/Movement.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-    public float speed = 1;
+    public float speed;
     public Rigidbody player;
     public Vector3 player_movement;
 
     // Use this for initialization
     void Start () {
+        speed = 2;
         player = GetComponent<Rigidbody>();
     }
 
@@ -21,33 +22,38 @@ public class Movement : MonoBehaviour {
             print("WHAT");
         } else
         {
-            if(Input.GetKey("w"))
+            if (Input.GetKey("w"))
             {
-                player_movement.Set(10 * speed, -5*speed, player_movement.z);
-                transform.rotation = player_rotation * Quaternion.Euler(0,0,1);
+                player_movement.Set(10 * speed, -5 * speed, player_movement.z);
+                transform.rotation = player_rotation * Quaternion.Euler(transform.rotation.x, transform.rotation.y, -0.25f * speed);
             }
             else if (Input.GetKey("s"))
             {
-                player_movement.Set(10 * speed, 5*speed, player_movement.z);
-                transform.rotation = player_rotation * Quaternion.Euler(0, 0, -1);
+                player_movement.Set(10 * speed, 5 * speed, player_movement.z);
+                transform.rotation = player_rotation * Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0.25f * speed);
             } else
             {
                 player_movement.Set(10 * speed, 0, player_movement.z);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, Time.deltaTime / (1f/speed));
             }
 
             if (Input.GetKey("a"))
             {
                 player_movement.Set(10 * speed, player_movement.y, 5 * speed);
-                transform.rotation = player_rotation * Quaternion.Euler(1, 0, 0);
+                transform.rotation = player_rotation * Quaternion.Euler(0.25f*speed, transform.rotation.y, transform.rotation.z);
             }
             else if (Input.GetKey("d"))
             {
                 player_movement.Set(10 * speed, player_movement.y, -5 * speed);
-                transform.rotation = player_rotation * Quaternion.Euler(-1, 0, 0);
+                transform.rotation = player_rotation * Quaternion.Euler(-0.25f*speed, transform.rotation.y, transform.rotation.z);
             }
             else
             {
                 player_movement.Set(10 * speed, player_movement.y, 0);
+                //Quaternion test = new Quaternion();
+                //test.Set(0, 0, 0, 0);
+                // transform.rotation = test;
+                //transform.rotation = player_rotation * Quaternion.Euler(0, transform.rotation.y, transform.rotation.z);
             }
             player.velocity = player_movement;
         }
