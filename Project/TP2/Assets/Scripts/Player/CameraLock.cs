3740245c258156a21cc18f6b2e772f7997553227
hexco <hexco.kp@gmail.com>
2016-11-10 00:38:26 -0500

    Camera
    Belle Camera.

---
+++ Project/TP2/Assets/Scripts/Player/CameraLock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLock : MonoBehaviour {
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+   
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        float currentRotationAngle = transform.eulerAngles.y;
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+        transform.position = target.transform.position;
+        transform.position -= currentRotation * Vector3.forward * 10f;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        transform.LookAt(target.transform.position);
+	}
+}
