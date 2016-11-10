a70ace50944bf1879ccf10cbab536a857f651a53
Nick P <nicolas.papanicolaou20@gmail.com>
2016-11-09 19:27:14 -0500

    Push Mes shit

---
+++ Project/TP2/Assets/Scripts/Gameplay/ObstacleMouvement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleMouvement : MonoBehaviour {
+
+	static bool alreadyMoved = false;
+		
+	// Update is called once per frame
+	void Start () {
+		InvokeRepeating ("caca",0.0f,2.0f);
+	}
+
+	void caca(){
+		if (alreadyMoved) {
+			this.transform.Translate (0,0,-10.0f);
+			alreadyMoved = false;
+		} else {
+			this.transform.Translate (0,0,10.0f);
+			alreadyMoved = true;
+		}
+
+	}
+}
---
+++ Project/TP2/Assets/Scripts/Gameplay/Ring.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ring : MonoBehaviour {
+
+	void Update(){
+		
+	}
+
+	IEnumerator OnTriggerEnter(Collider other)
+	{
+		PickUpSound ();
+		Movement.speed += 5;
+		yield return new WaitForSeconds (1.5f);
+		Movement.speed -= 5;
+
+		//Destroy (this.gameObject);
+	}
+
+
+	void PickUpSound(){
+		AudioSource sound = GameObject.Find ("RingSound").GetComponent<AudioSource> ();
+		sound.Play ();
+	}
+}
