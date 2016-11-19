73aa3f7e0fc3821616028723053b3408ae94272b
hexco <hexco.kp@gmail.com>
2016-11-19 03:37:41 -0500

    Minor change
    Changement mineur sur le UI mobile
    Debut du tutoriel multiplateforme

---
+++ Project/TP2/Assets/Scripts/Scene/Tutorial/Platform_Manager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Platform_Manager : MonoBehaviour {
+    public Text Mobile_Text;
+    public Text PC_Text;
+
+	// Use this for initialization
+	void Start () {
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            this.gameObject.GetComponent<Text>().text = Mobile_Text.text;
+        }
+        else
+        {
+            this.gameObject.GetComponent<Text>().text = PC_Text.text;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
