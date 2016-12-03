a5fb7a275077c5fd01b1f43ca3f84f621e369947
hexco <hexco.kp@gmail.com>
2016-12-03 00:04:46 -0500

    Boss_4_p2
    Mechanic avance.

--- Project/TP2/Assets/Prefab/Scene/Stage_Four/Boss_4.cs
+++ Project/TP2/Assets/Prefab/Scene/Stage_Four/Boss_4.cs
@@ -5,6 +5,7 @@ public class Boss_4 : MonoBehaviour {
     public GameObject shield;
     public GameObject dummy;
     public GameObject shield_power;
+    public static bool boss_4 = false;
 
     private float hp;
 
@@ -18,6 +19,11 @@ public class Boss_4 : MonoBehaviour {
     public Transform shield_power_01_location;
 
     public GameObject boss;
+    public GameObject weapon;
+    private GameObject weapon_shot = null;
+    public Transform weapon_location;
+    public Transform weapon_target;
+    private bool Laser_Cool = true;
 
     // Use this for initialization
     void Start () {
@@ -29,6 +35,8 @@ public class Boss_4 : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        this.gameObject.transform.LookAt(weapon_target);
+
         if (this.GetComponent<EnemyHp>().hp <= hp)
         {
             hp = this.GetComponent<EnemyHp>().hp;
@@ -42,19 +50,53 @@ public class Boss_4 : MonoBehaviour {
             }
         }
 
-        if (shield_power_active_00 == null && shield_power_active_01 == null)
+        if ((shield_power_active_00 == null || shield_power_active_01 == null) && boss != null)
         {
             Destroy(active_shield);
             StartCoroutine(Shield_Respawn());
-        }      
+        }
+
+        if (boss_4)
+        {
+            if (weapon_shot != null)
+            {
+                weapon_shot.GetComponent<Rigidbody>().AddForce(transform.forward * 8, ForceMode.VelocityChange);
+            }
+            else if (Laser_Cool)
+            {
+                Laser_Cool = false;
+                StartCoroutine(Laser_CD());
+            }
+        }
     }
 
     IEnumerator Shield_Respawn()
     {
-        shield_power_active_00 = dummy;
-        yield return new WaitForSeconds(4.0f);
+        if (!shield_power_active_00)
+        {
+            shield_power_active_00 = dummy;
+        }
+
+        if (!shield_power_active_01)
+        {
+            shield_power_active_01 = dummy;
+        }
+
+        yield return new WaitForSeconds(3.0f);
         shield_power_active_00 = Instantiate(shield_power, shield_power_00_location.position, shield_power.transform.rotation) as GameObject;
+        yield return new WaitForSeconds(3.0f);
         shield_power_active_01 = Instantiate(shield_power, shield_power_01_location.position, shield_power.transform.rotation) as GameObject;
-        active_shield = Instantiate(shield, shield_location.position, shield.transform.rotation) as GameObject;
+
+        if((shield_power_active_00 && shield_power_active_00.tag == "Destroyable") && (shield_power_active_01 && shield_power_active_01.tag == "Destroyable"))
+        {
+            active_shield = Instantiate(shield, shield_location.position, shield.transform.rotation) as GameObject;
+        }
+    }
+
+    IEnumerator Laser_CD()
+    {
+        yield return new WaitForSeconds(2.0f);
+        weapon_shot = Instantiate(weapon, weapon_location.position, weapon.transform.rotation) as GameObject;
+        Laser_Cool = true;
     }
 }
--- Project/TP2/Assets/Scripts/Gameplay/ArrivingToBoss.cs
+++ Project/TP2/Assets/Scripts/Gameplay/ArrivingToBoss.cs
@@ -2,10 +2,18 @@
 using System.Collections;
 
 public class ArrivingToBoss : MonoBehaviour {
+    public bool stage_1;
+    public bool stage_2;
+    public bool stage_3;
+    public bool stage_4;
 
-	void OnTriggerEnter (Collider other) {
+    void OnTriggerEnter (Collider other) {
         if (other.tag == "Player")
         {
+            if (stage_4)
+            {
+                Boss_4.boss_4 = true;
+            }
             Player.moveForwardSpeed = 0;
         }
     }
